0483514cb7da53af1e81a1e4689f08cbf53d1be5
dandumitriu33 <dandumitriu33@gmail.com>
2020-04-23 11:24:29 +0300

    Add solutions -Fibonacci

---
+++ src/main/java/com/codecool/Fibonacci.java
@@ -0,0 +1,8 @@
+package com.codecool;
+
+public class Fibonacci {
+
+    public Fibonacci() {
+
+    }
+}
--- src/main/java/com/codecool/Streams.java
+++ src/main/java/com/codecool/Streams.java
@@ -1,10 +1,10 @@
 package com.codecool;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
 
 public class Streams {
     public static void main(String[] args) {
@@ -13,22 +13,25 @@ public class Streams {
          */
 
         String[] a1 = {"one", "two", "three"};
-        Stream<String> s1 = /* ??? */;
+        Stream<String> s1 = Arrays.stream(a1);
 
         /*
          * Create a stream containing the Strings "one" , "two" and "three"
          * without using an array
          */
 
-        Stream<String> s2 = /* ??? */;
+        Stream<String> s2 = Stream.of("one", "two", "three");
 
         /*
          * Create a stream using a stream builder.
          */
 
-        Stream.Builder<String> b1 = /* ??? */;
+        Stream.Builder<String> b1 = Stream.builder();
         /* ??? */
-        Stream<String> s3 = /* ??? */;
+        Stream<String> s3 = b1.add("one")
+                              .add("two")
+                              .add("three")
+                              .build();
 
         /*
          * Collect one of the above defined streams into a list.
@@ -36,7 +39,8 @@ public class Streams {
          * HINT: the keyword here is "collect"
          */
 
-        List<String> l1 = /* ??? */;
+        List<String> l1 =
+                s3.collect(Collectors.toList());
 
         /*
          * Streams can be infinite.  We obviously cannot create such a
@@ -54,13 +58,13 @@ public class Streams {
 
         Integer twoToTheZeroth = 1;
         UnaryOperator<Integer> doubler = (Integer x) -> 2 * x;
-        Stream<Integer> s4 = /* ??? */;
+        Stream<Integer> s4 = Stream.iterate(twoToTheZeroth, doubler);
 
         /*
          * Create a stream containing the first ten elements of s4.
          */
 
-        Stream<Integer> s5 = s4. /* ??? */;
+        Stream<Integer> s5 = s4.limit(10);
 
         /*
          * Create a stream containing the elements of the Fibonacci
@@ -69,7 +73,7 @@ public class Streams {
          * HINT: You will need to create a new class for this.
          */
 
-        Supplier<Integer> fibSupp = new Fibonacci();
-        Stream<Integer> s6 = /* ??? */;
+//        Supplier<Integer> fibSupp = new Fibonacci();
+//        Stream<Integer> s6 = ;
     }
 }
